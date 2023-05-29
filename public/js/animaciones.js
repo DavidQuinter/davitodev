d7eb0bc11cf22378047728f408b54a8bfb33000c
david <davidquinterocentro@hotmail.com>
2023-05-29 16:13:34 -0600

    terminado 90%

--- public/js/animaciones.js
+++ public/js/animaciones.js
@@ -4,8 +4,23 @@ window.sr = ScrollReveal({ mobile: true });
         origin: 'right',
         distance: '-150px'
     });
-    sr.reveal('.main',{
+    sr.reveal('.home',{
         duration: 3000,
         origin: 'right',
+        distance: '-150px'
+    });
+    sr.reveal('.contact',{
+        duration: 3000,
+        origin: 'top',
+        distance: '-150px'
+    });
+    sr.reveal('.projects',{
+        duration: 3000,
+        origin: 'rigth',
+        distance: '-150px'
+    });
+    sr.reveal('.about',{
+        duration: 3000,
+        origin: 'left',
         distance: '-150px'
     });
